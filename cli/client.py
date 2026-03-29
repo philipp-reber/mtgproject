51bf0cdc3f57923bec507daeb5e9d938117daf3e
philipp-reber <ph.reber@gmail.com>
2026-03-29 14:14:47 +0200

    updated CLI functionalities

--- cli/client.py
+++ cli/client.py
@@ -1,20 +1,37 @@
 import argparse
 
 from lib.extract_utils import(
-    check_data_updates,
+    check_status,
+    download_data,
+)
+from lib.mongo_utils import(
+    populate_raw_db,
+)
+
+from lib.general_utils import(
+    start_docker_containers,
 )
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Magic Data and Pricing CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
-    subparsers.add_parser("checkupdates", help="Check if new raw data is available")
+    subparsers.add_parser("checkstatus", help="Check if new raw data is available")
+    subparsers.add_parser("downloaddata", help="Download the raw bulk data")
+    subparsers.add_parser("startdocker", help="Starts the docker containers for this project through the compose file")
+    subparsers.add_parser("populateraw", help="Populates the raw MongoDB Container with data")
 
     args = parser.parse_args()
 
     match args.command:
-        case "checkupdates":
-            check_data_updates()
+        case "checkstatus":
+            check_status()
+        case "downloaddata":
+            download_data()
+        case "startdocker":
+            start_docker_containers()                
+        case "populateraw":
+            populate_raw_db()
         case _:
             parser.print_help()
 
